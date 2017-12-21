7fd9e5140c6a61217b9b3ddb825b5edbdb55ab25
Gabriel Angelo <gabriel.angelo91@gmail.com>
2017-12-21 11:31:40 -0300

    feat: created route for handling users

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,49 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+const Users = require('../models/index').Users
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
+router.get('/', function(req, res, next){
+  Users.findAll()
+  .then(users => res.json({ users }))
+  .catch(error => res.status(500).json(error))
+})
+
+router.get('/:id', (req, res) => {
+  Users.findById(req.params.id)
+  .then(user => res.json({ user }))
+  .catch(error => res.status(500).json(error))
+})
+
+router.post('/', (req, res) => {
+  const user = {
+      name: req.body.name,
+      email: req.body.email,
+      password: req.body.password,
+  }
+
+  Users.create(user)
+  .then(user => res.json(user))
+  .catch(error => res.status(500).json(error))
+})
+
+router.put('/:id', (req, res) => {
+  const userUpdated = {
+    name: req.body.name,
+    email: req.body.email,
+    password: req.body.password,
+  }
+
+  Users.findById(req.params.id)
+  .then(user => user.update(userUpdated))
+  .then(user => res.json(user))
+  .catch(error => res.status(500).json(error))
+})
+
+router.delete('/:id', (req, res) => {
+  Users.findById(req.params.id)
+  .then(user => user.destroy())
+  .then(() => res.json())
+  .catch(error => res.status(500).json(error))
+})
 
 module.exports = router;
