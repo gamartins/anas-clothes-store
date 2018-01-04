06b99ecbc9830c4a8bb035fb7e4efd75e6f52458
Gabriel Angelo <gabriel.angelo91@gmail.com>
2018-01-04 11:15:09 -0300

    feat: configured basic corrs

--- app.js
+++ app.js
@@ -20,6 +20,10 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
+// setup cors
+var cors = require('cors')
+app.use(cors())
+
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
